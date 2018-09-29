49dab359a30de010f9fc9b76405553c07f7820e8
Justin Isrow <DoctorAtom7@gmail.com>
2018-09-28 21:50:20 -0400

    The method Ninety_Nine_Bottles_of_Beer has been implemented in BeerSong.java.

--- BeerSong.java
+++ BeerSong.java
@@ -3,5 +3,19 @@ public class BeerSong {
 	}
 	
 	public static void Ninety_Nine_Bottles_of_Beer() {
+		for (int i = 99; i > 0; i--) {
+			String bottles = String.valueOf(i);
+			if (i == 1) {
+				System.out.println(bottles + " bottle of beer on the wall, " + bottles + " bottle of beer");
+				System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+			} else {
+				System.out.println(bottles + " bottles of beer on the wall, " + bottles + " bottles of beer");
+				if (i == 2) {
+					System.out.println("take one down, pass it around, " + String.valueOf(i - 1) + " bottle of beer on the wall.");
+				} else {
+					System.out.println("take one down, pass it around, " + String.valueOf(i - 1) + " bottles of beer on the wall.");
+				}
+			}
+		}
 	}
 }
